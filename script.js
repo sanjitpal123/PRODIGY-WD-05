108513b47100b0b0f54ebe1fef653b804a3c3c73
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-06-08 20:55:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const temperature = document.querySelector('.temperature');
 const description = document.querySelector('.description');
 const humidity = document.getElementById('humidity');
 const wind_speed = document.getElementById('wind-speed'); 
-
+ 
 const location_not_found = document.querySelector('.location-not-found');
 
 const weather_body = document.querySelector('.weather-body');
